387e16c5fe35eac000fe7d3cdb939e472c7c2cd4
ljj430 <ljj790225650@gmail.com>
2023-06-27 17:00:31 -0400

    empty_catch

---
+++ code_with_warnings/src/main/java/org/example/EmptyCatchBlock.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class EmptyCatchBlock {
+    public static void main(String[] args) {
+        try {
+            int a = 1;
+            int b = 0;
+            int c = a/b;
+            System.out.println(c);
+        } catch(ArithmeticException ae) {
+            // An empty catch block
+            //System.err.println("ArithmeticException: " + e.getMessage());
+        }
+    }
+}
---
+++ code_with_warnings/src/main/java/org/example/OtherViolations.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class OtherViolations {
+    public void method1 (){
+        int a = 0;
+        int b = 0;
+        int c = 0;
+    }
+
+}
